1f0ec4aae3eae6509c8c8a83ec55d4149bde4ba6
Adeliaa <140463906+adeliaamantaa@users.noreply.github.com>
2026-02-21 01:07:20 +0700

    feat: add default and parameterized constructor usage in main

--- Jobsheet-2-Object/MahasiswaMain01.java
+++ Jobsheet-2-Object/MahasiswaMain01.java
@@ -7,15 +7,23 @@ public class MahasiswaMain01 {
         mhs1.nama  = "Adelia Amanta Putri";
         mhs1.kelas = "SIB1D";
         mhs1.ipk   = 4.00;
-
         System.out.println("===== Informasi Pertama =====");
+        
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("SIB2D");
         mhs1.updateIpk(3.85);
-
-        System.out.println("\n===== Informasi Kedua =====");
+        System.out.println("\n===== Informasi Pertama (setelah update) =====");
+        
         mhs1.tampilkanInformasi();
-
         System.out.println("\nNilai Kinerja: " + mhs1.nilaiKinerja(mhs1.ipk));
+
+        System.out.println("\n===== Informasi Kedua (Konstruktor Default) =====");
+        Mahasiswaa01 mhs2 = new Mahasiswaa01();
+        mhs2.tampilkanInformasi();
+
+        System.out.println("\n===== Informasi Ketiga (Konstruktor Berparameter) =====");
+        Mahasiswaa01 mhs3 = new Mahasiswaa01("0987654321", "Adengg", "TI1D", 4.00);
+        mhs3.updateIpk(4.00);
+        mhs3.tampilkanInformasi();
     }
 }
