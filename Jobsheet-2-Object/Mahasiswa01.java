cce8b77a13acb8a4463a06c7dce4b9cef4238eb1
Adeliaa <140463906+adeliaamantaa@users.noreply.github.com>
2026-02-21 00:40:53 +0700

    Change method visibility to public in Mahasiswa01

--- Jobsheet-2-Object/Mahasiswa01.java
+++ Jobsheet-2-Object/Mahasiswa01.java
@@ -5,32 +5,32 @@ public class Mahasiswa01 {
     String kelas;
     double ipk;
 
-    Mahasiswa01() {
+    public Mahasiswa01() {
         nim   = "-";
         nama  = "-";
         kelas = "-";
         ipk   = 0.0;
     }
 
-    Mahasiswa01(String nim, String nama, String kelas, double ipk) {
+    public Mahasiswa01(String nim, String nama, String kelas, double ipk) {
         this.nim   = nim;
         this.nama  = nama;
         this.kelas = kelas;
         this.ipk   = ipk;
     }
   
-    void tampilkanInformasi() {
+    public void tampilkanInformasi() {
         System.out.println("NIM   : " + nim);
         System.out.println("Nama  : " + nama);
         System.out.println("Kelas : " + kelas);
         System.out.println("IPK   : " + ipk);
     }
 
-    void ubahKelas(String kelasBaru) {
+    public void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
 
-    void updateIpk(double ipkBaru) {
+    public void updateIpk(double ipkBaru) {
         if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
             ipk = ipkBaru;
         } else {
@@ -38,7 +38,7 @@ public class Mahasiswa01 {
         }
     }
 
-    String nilaiKinerja(double ipk) {
+    public String nilaiKinerja(double ipk) {
         if (ipk >= 3.5) {
             return "Cumlaude";
         } else if (ipk >= 3.0) {
