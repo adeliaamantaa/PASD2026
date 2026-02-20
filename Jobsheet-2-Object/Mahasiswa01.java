69098d61fa5d9d6e56d50173a2500f87821d2299
Adeliaa <140463906+adeliaamantaa@users.noreply.github.com>
2026-02-20 23:07:43 +0700

    Add Mahasiswa class with attributes and methods

---
+++ Jobsheet-2-Object/Mahasiswa01.java
@@ -0,0 +1,38 @@
+public class Mahasiswa01 {
+  
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja(double ipk) {
+        if (ipk >= 3.5) {
+            return "Cumlaude";
+        } else if (ipk >= 3.0) {
+            return "Memuaskan";
+        } else if (ipk >= 2.5) {
+            return "Cukup";
+        } else {
+            return "Kurang";
+        }
+    }
+}
