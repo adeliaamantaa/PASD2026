2ea75e22f3524f6351013db2a02398f6cd819a5b
Adeliaa <140463906+adeliaamantaa@users.noreply.github.com>
2026-06-14 20:53:14 +0700

    Add binary search method for IPK lookup

--- Jobsheet-6-Searching/MahasiswaBerprestasi.java
+++ Jobsheet-6-Searching/MahasiswaBerprestasi.java
@@ -47,4 +47,18 @@ public class MahasiswaBerprestasi {
         }
     }
 
+    int findBinarySearch(double cari, int left, int right) {
+        if (right >= left) {
+            int mid = (left + right) / 2;
+            if (cari == listMhs[mid].ipk) {
+                return mid;
+            } else if (listMhs[mid].ipk > cari) {
+                return findBinarySearch(cari, left, mid - 1);
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+
 }
